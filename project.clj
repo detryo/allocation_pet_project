bb23acc984fb3f728e96d80715e087eabff478f0
Christian <chris.sedano.arenas@gmail.com>
2017-07-21 16:33:35 +0100

    Add project description
    This is a dummy commit to practice git

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject allocation_clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Allocation Clojure pet project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
