ba7db5f1ad0ea7600a235ccd79f34e7fdf7a1aa0
Christian <chris.sedano.arenas@gmail.com>
2017-07-21 16:50:05 +0100

    Adjust project description
    Incorporating pull request comments

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject allocation_clj "0.1.0-SNAPSHOT"
-  :description "Allocation Clojure pet project"
+  :description "Allocation project in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
